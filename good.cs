ade12473187656581a6e41c90e91c645bc2b96e3
Louis BODAL <louis.bodal@expaceo.com>
2022-12-19 14:30:04 +0100

    good

---
+++ good.cs
@@ -0,0 +1,19 @@
+[assembly: ApiController]
+namespace Hubbix.Batiment.GestionPrestation.API;
+
+internal static class Program
+{
+  public static void Main(string[] args)
+  {
+    CreateHostBuilder(args)
+      .Build()
+      .Initialize()
+      .Run();
+  }
+
+  public static IHostBuilder CreateHostBuilder(string[] args)
+  {
+    return Host.CreateDefaultBuilder(args)
+      .UseHttp<Startup>();
+  }
+}
